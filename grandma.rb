e92be10b67df765074c7c8d50e11196f450699f2
Steven Senkowski <scsenkowski@gmail.com>
2017-11-03 18:11:07 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -2,10 +2,10 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase == "#{phrase}".upcase
-    return "HUH?! SPEAK UP, SONNY!"
   else phrase == "#{phrase}"
     return "NO, NOT SINCE 1938!"
+  elsif phrase == "#{phrase}".upcase
+    return "HUH?! SPEAK UP, SONNY!"
   end
 end
 # Whatever you say to grandma, she should respond with
