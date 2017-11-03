1739450dddc4624d3172427bc5e1898475bd19ba
Steven Senkowski <scsenkowski@gmail.com>
2017-11-03 18:10:40 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  eslif phrase == "#{phrase}".upcase
+  elsif phrase == "#{phrase}".upcase
     return "HUH?! SPEAK UP, SONNY!"
   else phrase == "#{phrase}"
     return "NO, NOT SINCE 1938!"
